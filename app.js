44586e096a6ac1a58d47dddb1596ebf0afb271a5
Mridsole <mridsole@gmail.com>
2015-07-01 00:22:53 +1000

    =yer

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('The server is listening at http://%s:%s', host, port);;
+  console.log('The server is listening at http://%s:%s', host, port);
 })
--- views/index.js
+++ views/index.js
@@ -4,11 +4,11 @@
 
   // some sample graph data
   var graphData = [
-    { id: 1, text: "DAVE SUCKS", gx: 0, gy: 0, connect: [] },
-    { id: 2, text: "yer true", gx: 0, gy: 20, connect: [ 1 ] },
-    { id: 3, text: "na", gx: 0, gy: 10, connect: [ 1, 2 ] },
-    { id: 4, text: "na ur shit", gx: 0, gy: 30, connect: [ 2 ] },
-    { id: 5, text: "yer", gx: 0, gy: 50, connect: [ 3, 4 ] }
+    { id: 1, text: "node 1", gx: 0, gy: 0, connect: [] },
+    { id: 2, text: "node 2", gx: 0, gy: 20, connect: [ 1 ] },
+    { id: 3, text: "node 3", gx: 0, gy: 10, connect: [ 1, 2 ] },
+    { id: 4, text: "node 4", gx: 0, gy: 30, connect: [ 2 ] },
+    { id: 5, text: "node 5", gx: 0, gy: 50, connect: [ 3, 4 ] }
   ];
 
   var app = angular.module('webbitClient', [  ]);
@@ -16,11 +16,6 @@
   app.controller('WebbitController', function () {
 
     this.graphData = graphData;
-
-    // assign z-index equal to id for each node (used for stacking)
-    this.setZIndices = function() {
-
-    };
   });
 
 })();
