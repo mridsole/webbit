328a6d0eb0d620dfd6b962f585dea12d6f4d9a6a
David <davidcoates95@gmail.com>
2015-07-01 10:28:31 +1000

    Revert "yer5000"
    This reverts commit 1d25ae7004e2b21e14af7301241ad70b1ddba745.

--- views/index.js
+++ views/index.js
@@ -4,11 +4,11 @@
 
   // some sample graph data
   var graphData = [
-    { id: 1, text: "node 1", gx: 0, gy: 0, class: 'webbit-node', connect: [] },
-    { id: 2, text: "node 2", gx: 250, gy: 20, class: 'webbit-node', connect: [ 1 ] },
-    { id: 3, text: "node 3", gx: 100, gy: 10, class: 'webbit-node', connect: [ 1, 2 ] },
-    { id: 4, text: "node 4", gx: 0, gy: 230, class: 'webbit-node', connect: [ 2 ] },
-    { id: 5, text: "node 5", gx: 0, gy: 450, class: 'webbit-node', connect: [ 3, 4 ] }
+    { id: 1, text: "node 1", gx: 0, gy: 0, connect: [] },
+    { id: 2, text: "node 2", gx: 0, gy: 20, connect: [ 1 ] },
+    { id: 3, text: "node 3", gx: 0, gy: 10, connect: [ 1, 2 ] },
+    { id: 4, text: "node 4", gx: 0, gy: 30, connect: [ 2 ] },
+    { id: 5, text: "node 5", gx: 0, gy: 50, connect: [ 3, 4 ] }
   ];
 
   var app = angular.module('webbitClient', [  ]);
@@ -16,29 +16,6 @@
   app.controller('WebbitController', function () {
 
     this.graphData = graphData;
-
-    // return a style object detailing the global coords of the node
-    this.style_nodeCoords = function(node) {
-      return { left: node.gx, top: node.gy };
-    };
-
-    this.mouseover = function(event, node) {
-      node.class = 'webbit-node-hover';
-    };
-
-    this.mouseleave = function(event, node) {
-      node.class = 'webbit-node';
-    };
   });
-
-  // element directive for a node
-  app.directive('webbitNode', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'directives/webbit-node.html'
-    };
-  });
-
-
 
 })();
